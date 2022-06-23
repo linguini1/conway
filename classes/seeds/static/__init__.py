54879116eeb6a08af0875cc19531c864137c0b5f
linguini1 <matteo.golin@gmail.com>
2022-06-23 12:25:27 -0400

    Added beehive and loaf shapes.

--- classes/seeds/static/__init__.py
+++ classes/seeds/static/__init__.py
@@ -8,7 +8,7 @@ from classes.seeds.seed import Seed
 # Classes
 class SquareSeed(Seed):
 
-    """Radiation-symbol looking seed."""
+    """Square, static shape."""
 
     def __init__(self):
         super().__init__()
@@ -16,4 +16,29 @@ class SquareSeed(Seed):
         self.coordinates = [
             (0, 0), (0, 1),
             (1, 0), (1, 1)
+        ]
+
+
+class BeehiveSeed(Seed):
+
+    """Beehive shape from origin COGL."""
+
+    def __init__(self):
+        super().__init__()
+
+        self.coordinates = [
+            (-1, 0), (0, 1), (1, 1),
+            (0, -1), (1, -1), (2, 0)
+        ]
+
+
+class LoafSeed(Seed):
+    """Loaf shape from origin COGL."""
+
+    def __init__(self):
+        super().__init__()
+
+        self.coordinates = [
+            (0, 0), (-1, 1), (1, -1),
+            (0, 2), (1, 2), (2, 1), (2, 0)
         ]
