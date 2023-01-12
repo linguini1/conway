bcd412ff2ba18dd9a715e543cf9c24d043c77d3e
Matteo Golin <matteo.golin@gmail.com>
2023-01-12 10:41:02 -0500

    Changed docstrings and types to be clearer.

--- utils/__init__.py
+++ utils/__init__.py
@@ -30,7 +30,7 @@ def export_seed(seed: list[Coordinates]) -> None:
 
 def previous_seed() -> list[Coordinates]:
 
-    """Returns the previously used seed from the file."""
+    """Returns the previously used seed from the storage file."""
 
     with open(SEED_FILE_NAME, 'r') as file:
         data = json.load(file)
@@ -38,7 +38,7 @@ def previous_seed() -> list[Coordinates]:
     return [tuple(coordinate) for coordinate in data]
 
 
-def map_center(dimensions: Coordinates) -> Coordinates:
+def map_center(dimensions: tuple[int, int]) -> Coordinates:
 
     """Returns the (approximate) center of the map from its dimensions as x, y coordinates."""
 
@@ -47,12 +47,15 @@ def map_center(dimensions: Coordinates) -> Coordinates:
     return round(x / 2), round(y / 2)
 
 
-def percentage_of_map(percentage: float, dimensions: Coordinates) -> int:
+def percentage_of_map(percentage: float, dimensions: tuple[int, int]) -> int:
+
+    """
+    Returns the number of cells required to cover the passed percentage of the map dimensions. Percentages should be
+    between 0 and 100.
+    """
 
     assert 0 < percentage <= 100, "Percentage must be greater than 0, up to 100."
 
     y, x = dimensions
-
-    """Returns the number of points required to cover the passed percentage of the map dimensions."""
 
     return round((percentage / 100) * x * y)
